0d30955df58c414ffc24fdfb03d2f274aa75bf67
aandr <a.andreev.qa@gmail.com>
2023-05-08 18:08:04 -0700

    HW 8 Aleks Andreev

---
+++ tests/AAB_HW_0508.py
@@ -0,0 +1,17 @@
+import time
+import unittest
+
+from parameterized import parameterized
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
