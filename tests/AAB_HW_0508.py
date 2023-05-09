22fe3073d9e96dea555433627eaaf08d1e61f03c
aandr <a.andreev.qa@gmail.com>
2023-05-08 18:14:07 -0700

    HW 8 Aleks Andreev

--- tests/AAB_HW_0508.py
+++ tests/AAB_HW_0508.py
@@ -8,9 +8,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setUp(self) -> None:
+        browser = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
+        browser.get('http://hrm-online.portnov.com/')
+        self.browser = browser
+
+    def test_find_qa_manager(self):
+        browser = self.browser
 
 
 if __name__ == '__main__':
