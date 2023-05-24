54b80db5a2aeafb76656ffb3b2e206fa669ac04f
Hristo Buliev <hsbuliev@protonmail.com>
2023-05-23 17:56:29 -0700

    Hristo homework lesson 13

--- tests/homework_lesson13.py
+++ tests/homework_lesson13.py
@@ -1,6 +1,5 @@
 import unittest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
