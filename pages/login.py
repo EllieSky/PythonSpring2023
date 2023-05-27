6a2581c57934c74e3a3a303cc400144be1699c23
Olga <casianolga@gmail.com>
2023-05-26 19:52:21 -0500

    added logout to the login page

--- pages/login.py
+++ pages/login.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 from pages.base_page import BasePage
 from tests import BASE_URL
@@ -15,3 +17,13 @@ class LoginPage(BasePage):
         self.browser.find_element(By.ID, 'txtUsername').send_keys(user)
         self.browser.find_element(By.ID, 'txtPassword').send_keys(password)
         self.browser.find_element(By.ID, 'btnLogin').click()
+
+    def logout(self):
+        current_url = self.browser.current_url
+        self.browser.find_element(By.ID, 'welcome').click()
+        wait = WebDriverWait(self.browser, 3)
+        wait.until(
+            expected_conditions.visibility_of_element_located(
+                (By.LINK_TEXT, 'Logout'))).click()
+        wait.until(expected_conditions.url_changes(current_url),
+                   'url failed to change after logout was clicked')
